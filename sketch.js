98522d020632cf8249874b9e7b77105d6899f9b9
Unknown <marcus.belcastro1@education.nsw.gov.au>
2018-05-31 20:54:38 +1000

    Added constants and dev-note to header

--- sketch.js
+++ sketch.js
@@ -1,10 +1,31 @@
+// Main module by Marcus Belcastro
+// This is the main file for the program and is the first code to be executed
+
+// Global object lists
 var buttons = [];
 var sliders = [];
 var particles = [];
 var fields = [];
-var BASE_COL = 200;
-var LOCK_COL = 75;
-var TEXT_COL = 20;
+
+// Colours
+var BG_COL = 35;                  //background
+var BASE_COL = 200;               //base button/slider
+var LOCK_COL = 75;                //locked button/slider
+var TEXT_COL = 20;                //button/slider text
+var P_COL = 150;                  //particle
+var V_COL = 255;                  //vector
+var HYP_COL = [255, 117, 26]      //Hypotenuse
+
+//Vector character codes
+var VEL = 'V'
+var ACC = 'A'
+var FCE = 'F'
+
+// Numerical constants
+var P_RAD = 3;                    //particle radius
+var V_SCALE = 1;                  //vector scale
+var V_STROKE = 5;                 //vector stroke weight
+var T_SIZE = 16;                  //text size
 
 // Function to fix scrollbars on the canvas
 function windowResized() {
@@ -29,7 +50,7 @@ function mousePressed() {
 }
 
 function draw() {
-  background(35)
+  background(BG_COL)
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].show();
   }
