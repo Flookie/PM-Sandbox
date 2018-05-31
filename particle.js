3f4dcfd4ffe171c95ec86c8ed8b787cf2c1e8a13
Unknown <marcus.belcastro1@education.nsw.gov.au>
2018-05-31 20:54:53 +1000

    Finalised the particle module

--- particle.js
+++ particle.js
@@ -1,3 +1,6 @@
+// Particle Module by Marcus Belcastro
+// Object/class that manages individual particles and objects on the screen
+
 class Particle {
   constructor(posx, posy, v0x, v0y, mass, vectType) {
     this.x = posx
@@ -12,23 +15,73 @@ class Particle {
   }
 
   function applyField(field) {
-    this.a.add(field.a);
-    this.f.add(field.f);
+    this.a.add(field.a)
+    this.f.add(field.f)
   }
 
   function calcPos() {
-    //
+    this.x += this.v.x
+    this.y += this.v.y
   }
 
   function calcVelocity() {
-    //
+    this.v.add(this.a)
   }
 
   function calcAcceleration() {
-    //
+    this.a.add(vector(this.f.x / this.mass, this.f.y / this.mass))
   }
 
   function update() {
-    //
+    if (!this.frozen) {
+      this.calcAcceleration()
+      this.calcVelocity()
+      this.calcPos()
+      if (this.x < 0 || this.x > width) {
+        this.v.x *= -1
+      }
+      if (this.y < 0 || this.y > height) {
+        this.v.y *= -1
+      }
+      this.show()
+      this.a = vector(0, 0)
+      this.f = vector(0, 0)
+    }
+  }
+
+  function getVector() {
+    if (this.vectPointer == VEL) {
+      return this.v
+    } else if (this.vectPointer == ACC) {
+      return this.a
+    } else if (this.vectPointer == FCE) {
+      return this.f
+    }
+  }
+
+  function show() {
+    fill(P_COL)
+    ellipse(this.x, this.y, P_RAD)
+    if (this.showVectors) {
+      vect = this.getVector
+      tempx = vect.x
+      tempy = vect.y
+      coeffx = tempx * V_SCALE
+      coeffy = tempy * V_SCALE
+      strokeWeight(V_STROKE)
+      stroke(V_COL)
+      line(this.x, this.y, this.x+coeffx, this.y)
+      line(this.x, this.y, this.x, this.y+coeffy)
+      if (tempx != 0 && tempy != 0) {
+        stroke(HYP_COL[0], HYP_COL[1], HYP_COL[2])
+        line(this.x, this.y, this.x+coeffx, this.y+coeffy)
+        theta = invtan(abs(tempy/tempx))
+        textSize(T_SIZE)
+        //the operations with tempy here determine whether to place the theta
+        //text above or below the particle based on the sign of the vertical
+        //component of the vector.
+        text('°', this.x, 10*(tempy/abs(tempy)))
+      }
+    }
   }
 }
