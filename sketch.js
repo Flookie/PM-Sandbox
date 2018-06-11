a53bb3ce139bbf7d74a8f3b1071a272ef2cadc78
Unknown <marcus.belcastro1@education.nsw.gov.au>
2018-06-12 08:44:32 +1000

    Added slider click checking

--- sketch.js
+++ sketch.js
@@ -39,12 +39,14 @@ function windowResized() {
 function setup() {
   angleMode(DEGREES)
   createCanvas(windowWidth, windowHeight)
-  //sliders.push(new Slider(20, 20, 75, 275))
+  //sliders.push(new Slider(20, 20, 75, 475, 25, 125))
   //buttons.push(new Button(200, 200, 275, 75, function() {console.log("Clicked");}, 'TEXT'))
-  //fields.push(new Field(0.1, 1, 440))
+  //fields.push(new Field(0, 1, 270))
   //fields[0].hide = false
-  //particles.push(new Particle(0, 0, 1, 0, 10, VEL))
+  //particles.push(new Particle(100, height, 3, 10, 10, VEL))
+  //particles.push(new Particle(100, height, 3, 6, 20, VEL))
   //particles[0].showVectors = true
+  //particles[1].showVectors = true
 }
 
 function mousePressed() {
@@ -53,6 +55,9 @@ function mousePressed() {
     if (buttons[i].isClicked(mouseX, mouseY)) {
       buttons[i].do(buttons[i])
     }
+  }
+  for (let i = 0; i < sliders.length; i++) {
+    sliders[i].checkClicked()
   }
 }
 
